1d96e76b4aa064f41dd71f1ed1a783e344213a20
Alex <linyanshuo@ip148-137.wlan.ntust.edu.tw>
2017-12-27 14:42:49 +0800

    Finish

--- Tetris.java
+++ Tetris.java
@@ -12,7 +12,7 @@ public class Tetris {
 	UserEvents userEvents;
 	ConcreteTetrisModelSubject subject;
 	TetrisController controller;
-	
+	ConcreteTetrisViewObserver observer;
 	
 	Tetris(){
 		//subject為遊戲中的資料
@@ -20,7 +20,14 @@ public class Tetris {
 		//controller為控制遊戲流程
 		controller = new TetrisController(subject);
 		userEvents = new UserEvents(controller);
+		observer = new ConcreteTetrisViewObserver(subject);
 		
+		observer.addKeyListener(useoffsetrEvents);
+		
+		//將畫面與subject連結
+		subject.AddObserver(observer);
+		
+		observer.setVisible(true);
 
 
 	}
@@ -48,6 +55,39 @@ class UserEvents implements KeyListener {
     class TimeClock implements ActionListener{
         public void actionPerformed(ActionEvent e) {
         	controller.down_shift();
+        }
+    }
+  //在KeyListener中，監聽keyPressed事件
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()){
+        		//按上的話往右旋轉
+            case KeyEvent.VK_UP:
+            	controller.CW_rotate();
+                break;
+            //按下的話就往下一格
+            case KeyEvent.VK_DOWN:
+            	controller.down_shift();
+                break;
+            //按右的話往右一格
+            case KeyEvent.VK_RIGHT:
+            	controller.right_shift();
+                break;
+            //按左的話往左一格
+            case KeyEvent.VK_LEFT:
+            	controller.left_shift();
+                break;
+            //按space的時候直接不斷往下
+            case KeyEvent.VK_SPACE:
+            	controller.hard_drop();
+                break;
+            //按下Z的話往左旋轉
+            case KeyEvent.VK_Z:
+            	controller.CCW_rotate();
+                break;
+            //按下Shift的話，Hold著當前方塊
+            case KeyEvent.VK_SHIFT:
+            	controller.hold();
+                break;
         }
     }
 
